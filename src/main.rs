6b26fcd729d6f1ed74e320f9abe59339a97ed056
Peter Bjorklund <piot@hotmail.com>
2024-03-17 20:01:10 +0100

    chore: initial import

---
+++ src/main.rs
@@ -0,0 +1,134 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Peter Bjorklund. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+use std::fs;
+mod yaml;
+
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub enum CategoryType {
+    Changed,
+    Added,
+    Breaking,
+    Deprecated,
+    Docs,
+    Experimental,
+    Fixed,
+    Improved,
+    Noted,
+    Performance,
+    Refactored,
+    Removed,
+    Security,
+    Style,
+    Tests,
+    Unreleased,
+    Workaround,
+}
+
+#[derive(Debug, Clone)]
+struct CategoryInfo {
+    icon: &'static str,
+    description: &'static str,
+}
+
+
+fn info_from_category_name(name: CategoryType) -> CategoryInfo {
+    let lookup: std::collections::HashMap<CategoryType, CategoryInfo> = [
+        (CategoryType::Added, CategoryInfo { icon: "star2", description: "added" }),
+        (CategoryType::Changed, CategoryInfo { icon: "hammer_and_wrench", description: "changed" }),
+        (CategoryType::Fixed, CategoryInfo { icon: "lady_beetle", description: "fixed" }),
+        (CategoryType::Workaround, CategoryInfo { icon: "see_no_evil", description: "workaround" }),
+        (CategoryType::Performance, CategoryInfo { icon: "zap", description: "performance" }),
+        (CategoryType::Tests, CategoryInfo { icon: "vertical_traffic_light", description: "test" }),
+        (CategoryType::Removed, CategoryInfo { icon: "fire", description: "removed" }),
+        (CategoryType::Improved, CategoryInfo { icon: "art", description: "improved" }),
+        (CategoryType::Breaking, CategoryInfo { icon: "triangular_flag_on_post", description: "breaking" }),
+        (CategoryType::Deprecated, CategoryInfo { icon: "spider_web", description: "deprecated" }),
+        (CategoryType::Refactored, CategoryInfo { icon: "recycle", description: "refactor" }),
+        (CategoryType::Experimental, CategoryInfo { icon: "alembic", description: "experimental" }),
+        (CategoryType::Docs, CategoryInfo { icon: "book", description: "docs" }),
+        (CategoryType::Noted, CategoryInfo { icon: "beetle", description: "known issue" }),
+        (CategoryType::Style, CategoryInfo { icon: "gem", description: "style" }),
+        (CategoryType::Unreleased, CategoryInfo { icon: "soon", description: "unreleased" }),
+    ]
+    .iter()
+    .cloned()
+    .collect();
+
+    let info = lookup.get(&name).unwrap_or_else(|| {
+        panic!("unknown '{:?}'", name);
+    });
+
+    info.clone()
+}
+
+fn print_line(change_type: CategoryType, s: String) {
+    let info = info_from_category_name(change_type);
+    println!("* :{}: {} ", info.icon, s);
+}
+
+
+fn print_optional_list(change_type: CategoryType, list: Option<Vec<String>>) {
+    if let Some(items) = list {
+        for item in items {
+            print_line(change_type.clone(), item)
+        }
+    }
+}
+
+fn print_changes(changes: yaml::Changes) {
+    print_optional_list(CategoryType::Added, changes.added);
+    print_optional_list(CategoryType::Breaking, changes.breaking);
+    print_optional_list(CategoryType::Changed, changes.changed);
+    print_optional_list(CategoryType::Deprecated, changes.deprecated);
+    print_optional_list(CategoryType::Docs, changes.docs);
+    print_optional_list(CategoryType::Experimental, changes.experimental);
+    print_optional_list(CategoryType::Fixed, changes.fixed);
+    print_optional_list(CategoryType::Improved, changes.improved);
+    print_optional_list(CategoryType::Noted, changes.noted);
+    print_optional_list(CategoryType::Performance, changes.performance);
+    print_optional_list(CategoryType::Refactored, changes.refactored);
+    print_optional_list(CategoryType::Removed, changes.removed);
+    print_optional_list(CategoryType::Security, changes.security);
+    print_optional_list(CategoryType::Style, changes.style);
+    print_optional_list(CategoryType::Tests, changes.tests);
+    print_optional_list(CategoryType::Unreleased, changes.unreleased);
+    print_optional_list(CategoryType::Workaround, changes.workaround);
+}
+
+fn main() {
+    let file_path = "changelog.yaml";
+    let yaml_str = fs::read_to_string(file_path).expect("could not load file");
+
+    let deserialized: yaml::Document = serde_yaml::from_str(&yaml_str).unwrap();
+
+    println!("# Changelog\n");
+
+    for (release_version, release) in deserialized.releases {
+        let link_to_version = format!(
+            "https://github.com/{}/releases/tag/{}",
+            deserialized.repo, release_version
+        );
+
+        println!(
+            "# :bookmark: [{}]({}) ({})\n",
+            release_version, link_to_version, release.date
+        );
+        if let Some(notice) = release.notice {
+            println!("{}", notice);
+        }
+
+        for (section_name, section) in release.sections {
+            println!("\n### {}", section_name);
+
+            if let Some(notice) = section.notice {
+                println!("\n{}\n", notice);
+            }
+
+            print_changes(section.changes);
+        }
+    }
+}
---
+++ src/yaml.rs
@@ -0,0 +1,86 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Peter Bjorklund. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+use std::collections::HashMap;
+
+use indexmap::IndexMap;
+use serde::Deserialize;
+
+#[derive(Debug, PartialEq, Deserialize)]
+pub struct Changes {
+    // Added denotes new features or functionalities introduced in the software.
+    pub added: Option<Vec<String>>,
+
+    // Changed indicates changes to existing features or functionalities.
+    pub changed: Option<Vec<String>>,
+
+    // Deprecated signifies functionalities that are no longer recommended and will be removed in future versions.
+    pub deprecated: Option<Vec<String>>,
+
+    // Removed lists functionalities or features that have been removed from the software. Should have been set as Deprecated in version prior to being removed. Is implicitly breaking changes.
+    pub removed: Option<Vec<String>>,
+
+    // Fixed enumerates fixes for bugs or issues in the software.
+    pub fixed: Option<Vec<String>>,
+
+    // Security includes changes related to security enhancements or fixes.
+    pub security: Option<Vec<String>>,
+
+    // ---------------- Others ---------------
+
+    // Improved lists improvements made to existing functionalities without adding new features.
+    pub improved: Option<Vec<String>>,
+
+    // Workaround provides workarounds or temporary solutions for known issues or limitations.
+    pub workaround: Option<Vec<String>>,
+
+    // Tests includes changes or additions to testing procedures or test cases.
+    pub tests: Option<Vec<String>>,
+
+    // Docs lists changes or additions to documentation, such as README files or inline code comments.
+    pub docs: Option<Vec<String>>,
+
+    // Refactored denotes changes made to improve code structure or organization without changing external behavior.
+    pub refactored: Option<Vec<String>>,
+
+    // Performance includes changes aimed at improving the performance of the software.
+    pub performance: Option<Vec<String>>,
+
+    // Breaking denotes changes that may break backward compatibility with previous versions. Changed, but breaks the API compatibilty.
+    pub breaking: Option<Vec<String>>,
+
+    // Experimental lists experimental features or functionalities that are not yet stable or fully supported and might be removed with short or no notice in future versions.
+    pub experimental: Option<Vec<String>>,
+
+    // Noted provides a place to note any other significant changes not covered by the above categories.
+    pub noted: Option<Vec<String>>,
+
+    // Style denotes changes related to coding style, formatting, or other stylistic aspects.
+    pub style: Option<Vec<String>>,
+
+    // Unreleased contains a list of changes that are planned but not yet released in any version.
+    // These changes typically represent work that is in progress or pending release in a future version.
+    // Once a version is released, the changes listed in Unreleased are moved to the appropriate category (e.g., Added, Changed, Fixed, etc.).
+    pub unreleased: Option<Vec<String>>,
+}
+
+#[derive(Debug, PartialEq, Deserialize)]
+pub struct Section {
+    pub notice: Option<String>,
+    pub changes: Changes,
+}
+
+#[derive(Debug, PartialEq, Deserialize)]
+pub struct Release {
+    pub date: String,
+    pub notice: Option<String>,
+    pub sections: IndexMap<String, Section>,
+}
+
+#[derive(Debug, PartialEq, Deserialize)]
+pub struct Document {
+    pub repo: String,
+    pub releases: HashMap<String, Release>,
+}
